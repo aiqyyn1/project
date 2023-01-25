8c29baf88a05a64d651d5dae802dd2ccb59c4a4f
Aikyn <ibrayevaiqyn@gmail.com>
2023-01-25 12:55:26 +0600

    fixed

--- src/components/Reg.js
+++ src/components/Reg.js
@@ -7,18 +7,19 @@ import axios from 'axios';
 export default function Reg() {
 
 
-  // const [emptyFirstCheck, setFirstEmptyCheck]=useState(true);
-  // const [emptyLastCheck, setEmptyLastCheck]=useState(true)
+  const [emptyFirstCheck, setFirstEmptyCheck]=useState(true);
+  const [emptyLastCheck, setEmptyLastCheck]=useState(true)
   const [emptyLastText, setEmptyLastText]=useState('')
   const [emptyFirstText, setEmptyFirstText]=useState('')
   const  [checkValidateEmail, setCheckValidateEmail]=useState('');
-  // const [checkPassword, setCheckPassword]=useState(true);
+  const [checkPassword, setCheckPassword]=useState(true);
   const [errorPassword, setErrorPassword]=useState('')
-  // const [checkEmail, setcheckEmail]=useState(true);
+  const [checkEmail, setcheckEmail]=useState(true);
   const [data, setData]=useState({
     name:'',
     surname:'',
-    email : ''
+    email : '',
+    password: ''
   })
   
   const [lastName, setLastName]=useState('');
@@ -28,9 +29,9 @@ export default function Reg() {
 // const handlePasswordChange=(e)=>{
 //   setPassword(e.target.value);
 // }
-// const handleRepeatPassword=(e)=>{
-//   setRepeatPassword(e.target.value);
-// }
+const handleRepeatPassword=(e)=>{
+  setRepeatPassword(e.target.value);
+}
 const url="http://localhost:3000/registration"
 const  handle= (e) => {
     const newdata={...data}
@@ -38,16 +39,25 @@ const  handle= (e) => {
     setData(newdata)
     console.log(data)
 }
+let navigate=useNavigate();
 const sumbit= (e)=>{
-  e.preventDefault()
-  axios.post(url,{
+
+  e.preventDefault();
+  const emailChecker=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (data.password===repeatPassword && emailChecker.test(data.email) && data.name.length>0 && data.surname.length>0 ){
+
+   axios.post(url,{
     name:data.name,
-    surname:data.surname,
-    email:data.email
+     surname:data.surname,
+
+   email:data.email,
+     password:data.password
   })
   .then(res=>{
     console.log(res.data)
-  })
+   })
+   navigate("/Check")
+ }
 }
 // const handleLastChange = (e)=>{
 //   setLastName(e.target.value)
@@ -55,65 +65,7 @@ const sumbit= (e)=>{
 // const handleEmailChange = (e) => {
 //   setEmail(e.target.value)
 // }
-// let navigate=useNavigate();
-// const routerChange=(e)=>{
-// e.preventDefault();
-//   const emailChecker=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-//   if (firstName.length===0  ){
-//    setFirstEmptyCheck(false)
-//     setEmptyFirstText("Fill in this field")
-//     setTimeout(() => {
-    
-//       setFirstEmptyCheck(true);
-//     }, 3000);
-//       navigate('')
-//   }
-//   else{
-//     setFirstEmptyCheck(true)
-//     setEmptyFirstText("")
-//   }
-//   if (lastName.length===0){
-//     setEmptyLastCheck(false);
-//     setEmptyLastText("Fill in this field")
-//     setTimeout(() => {
-    
-//       setEmptyLastCheck(true);
-//     }, 3000);
-//   }
-//   else{
-//     setEmptyLastCheck(true);
-//     setEmptyLastText("")
-//   }
-// if (!emailChecker.test(email)){
-//     setCheckValidateEmail("Not Validated")
-//     setcheckEmail(false)
-//     setTimeout(() => {
-    
-//       setcheckEmail(true);
-//     }, 3000);
-//     navigate('');
-//   }
-//   else{
-//     setcheckEmail(true)
-//   }
-//   if (password !== repeatPassword ||  password.length===0 && repeatPassword.length===0){
-//     setCheckPassword(false)
-//     setErrorPassword('Passwords don"t match')
-//     setTimeout(() => {
-    
-//       setCheckPassword(true);
-//     }, 3000);
-//     navigate('');
-//   }
-//   else{
-//      setCheckPassword(true)
-//   }
-//  if (password===repeatPassword && emailChecker.test(email) && firstName.length>0 && lastName.length>0 ){
 
-//   navigate("/Check")
-//  }
-
-// }
 
   return (
   
@@ -134,38 +86,38 @@ const sumbit= (e)=>{
      <div className='templates'>
 <div>
   
-      <input placeholder='Enter Your Name' type='text' onChange={(e)=>handle(e)}  name='name' id='name' value={data.name.toString()} ></input>
-      {/* <div>{emptyFirstCheck? <div></div> : <div className='firstError'>{emptyFirstText}</div>}</div> */}
+      <input placeholder='Enter Your Name' type='text' onChange={(e)=>handle(e)}  name='name' id='name' value={data.name.toString()} required='required' ></input>
+      
       </div>
       <div>
-      <input placeholder='Enter Your Surname' type='text' name='surname'onChange={(e)=>handle(e)}  id='surname' value={data.surname.toString()} ></input>
-      {/* <div>{emptyLastCheck ? <div></div> : <div className='firstError'> {emptyLastText}</div>}</div> */}
+      <input placeholder='Enter Your Surname' type='text' name='surname'onChange={(e)=>handle(e)}  id='surname' value={data.surname.toString()} required='required' ></input>
+     
       </div>
       <div>
 
-    <input placeholder='Enter Your Email' name='email' type='email' onChange={(e)=>handle(e)}  id='email' value={data.email.toString()} ></input>
-       {/* <div>{checkEmail ? <div></div> : <div className='firstError'> {checkValidateEmail} </div> }</div> */}
-      </div>
-      {/* <div>
+    <input placeholder='Enter Your Email' name='email' type='email' onChange={(e)=>handle(e)}  id='email' value={data.email.toString()} required='required'></input>
 
-     <input placeholder='Enter Your Password' onChange={handlePasswordChange} name='password' type='password' ></input>
+      </div>
+      <div>
+
+     <input placeholder='Enter Your Password' onChange={(e)=>handle(e)} name='password' type='password'id='password' value={data.password} required='required'></input>
 
 </div> 
 <div>
 
 <input placeholder='Repeat Your Password' onChange={handleRepeatPassword} name='repPassword' type='password' required='required'></input>
-<div>{checkPassword ? <div></div> : <div className='firstError'>{errorPassword} </div>}</div>
+
 <PasswordChecklist
     rules={["match"]}
     minLength={5}
-    value={password}
+    value={data.password}
     valueAgain={repeatPassword}
     onChange={(isValid) => {}}
    />
-</div>  */}
+</div> 
      </div>
      <div className='button'>
-     <button name='regIn'>REG IN</button>
+     <button name='regIn' >REG IN</button>
      </div>
      </form>
    </div>
